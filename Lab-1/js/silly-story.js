7cf54e908c4bc563dda2718309cc5d2e9a5bcc45
yurika-s <200558167@student.georgianc.on.ca>
2024-09-20 18:21:11 -0400

    Added function for replacing items in the story and assigned it for a click event

--- Lab-1/js/silly-story.js
+++ Lab-1/js/silly-story.js
@@ -35,38 +35,44 @@ function randomValueFromArray(array) {
 
 /* STEP 6: Review the partially complete result() function below */
 function result() {
-	// STEP 7: Create a new variable called newStory and set it to the value of storyText - we don't want to overwrite the original story!
-
-	/* STEP 8: Use the randomValueFromArray() function to generate a value for each of three new variables - xItem, yItem, and zItem
+  // STEP 7: Create a new variable called newStory and set it to the value of storyText - we don't want to overwrite the original story!
+  let newStory = storyText;
+  /* STEP 8: Use the randomValueFromArray() function to generate a value for each of three new variables - xItem, yItem, and zItem
 	Call up the function and for each variable, pass it the array from which to grab a random string - for example if insertW was an array of strings, I would type:
-	var wItem = randomValueFromArray(insertW);
+	var wItem = randomValueFromArray(insertW); */
 
-	/* STEP 9: Replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. Each time, be sure to update the variable newStory (with +=). You might need to do one of the above replacements twice! */
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
 
-	/* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
-	if(customName.value != '') {
+  /* STEP 9: Replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem.
+   Each time, be sure to update the variable newStory (with +=). You might need to do one of the above replacements twice! */
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
 
-	}
-	/* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
-  if(document.getElementById("metric").checked) {
-		// STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
-		// STEP 11b: Replace the string 300 pounds with the updated weight in kg
-
-		// STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
-
-		// STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
-
+  /* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
+  if (customName.value != '') {
+    newStory = newStory.replace('Bob', customName.value);
   }
-	/* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
-
-	// The following line makes the paragraph visible
-	story.style.visibility = 'visible';
+  /* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
+  if (document.getElementById('metric').checked) {
+    // STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
+    let weight = 300 * 0.453592;
+    // STEP 11b: Replace the string 300 pounds with the updated weight in kg
+    newStory = newStory.replace('300 pounds', weight.toFixed(2) + ' kg');
+    // STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
+    let temp = ((94 - 32) * 5) / 9;
+    // STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
+    newStory = newStory.replace('94 fahrenheit', temp.toFixed(1) + ' °C');
+  }
+  /* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
+  story.textContent = newStory;
+  // The following line makes the paragraph visible
+  story.style.visibility = 'visible';
 }
 
 // EVENT LISTENERS
 /* STEP 5: Add a click event listener to the randomize variable so that when the button it represents is clicked, the result() function is run. */
-
-
+randomize.addEventListener('click', result);
 // This lab based on the excellent assessment challenge at https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator
-
-
