335d0511ad2e58a3a73ec26b06f670e0221444ff
yurika-s <200558167@student.georgianc.on.ca>
2024-10-08 16:10:09 -0400

    added declaration of constants and variables to js file

---
+++ Lab-2/js/page-color-changer.js
@@ -0,0 +1,11 @@
+// grab html, input slider elements
+const html = document.querySelector('html');
+const sliderElements = [
+  document.querySelector('#red'),
+  document.querySelector('#green'),
+  document.querySelector('#blue'),
+];
+
+// declare array to store RGB values
+let RGBValues = ['0', '0', '0'];
+
