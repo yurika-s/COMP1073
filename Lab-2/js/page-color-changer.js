4bfc6ab47d7b7879597c43a9e96a2e76cc1ee14a
yurika-s <200558167@student.georgianc.on.ca>
2024-10-08 16:10:50 -0400

    added functions to set RGB values and change background color

--- Lab-2/js/page-color-changer.js
+++ Lab-2/js/page-color-changer.js
@@ -9,3 +9,18 @@ const sliderElements = [
 // declare array to store RGB values
 let RGBValues = ['0', '0', '0'];
 
+// function to set RGB value depending on each slider value
+const setRGBValue = (index, element) => {
+  let inputValue = element.value;
+  RGBValues[index] = inputValue;
+};
+
+// function to change the html background color
+const changeBgColor = () => {
+  let bgValue = `rgb(${RGBValues[0]}, ${RGBValues[1]}, ${RGBValues[2]})`;
+  html.style.backgroundColor = bgValue;
+  if (bgValue === 'rgb(255, 255, 255)') {
+    html.style.color = '#000';
+  }
+};
+
